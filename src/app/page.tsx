c47d45093b4220022f2b98c2a343226bc133798f
kanwarsingh <kanwar16112309@gmail.com>
2024-03-04 20:19:29 -0600

    Positioning

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import Chat from "@/components/Chat";
 export default function Home() {
   return (
     <main className="min-w-screen min-h-screen bg-fixed bg-[url('../../public/philo.webp')] bg-cover bg-center flex flex-col">
-      <div className={"flex items-stretch mx-auto w-full max-w-[700px]"}>
+      <div className={"flex items-stretch mx-auto w-full max-w-[700px] my-auto"}>
         <Chat />
       </div>
     </main>
