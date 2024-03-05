888ccfe453db1916a0ab2789e67afb61b4eeb9e3
kanwarsingh <kanwar16112309@gmail.com>
2024-03-04 20:10:49 -0600

    Layout changes for responsiveness on phones

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,8 @@
 import Chat from "@/components/Chat";
 export default function Home() {
   return (
-    <main className="min-w-screen min-h-screen bg-fixed bg-[url('../../public/philo.webp')] bg-cover bg-center flex flex-col">
-      <div className={"flex items-stretch mx-auto my-auto w-full max-w-[700px]"}>
+    <main className="h-screen w-screen bg-fixed bg-[url('../../public/philo.webp')] bg-cover bg-center flex flex-col justify-center overflow-hidden">
+      <div className={"flex items-stretch mx-auto w-full max-w-[700px] my-auto h-full max-h-[600px]"}>
         <Chat />
       </div>
     </main>
